5bf1af4f6c7bfb84bb170701ebca5a4fa7b8e51b
Daniel Kanzel <danielkanzel@gmail.com>
2026-01-14 12:22:54 +0300

    Update build-binaries.sh

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -278,6 +278,9 @@ function build_binary() {
     LUA_BUILD_DIR="$(find "${SELF_PATH}/../libs/${ARCH_BUILD}/" -maxdepth 1 -type d -name "lua-*" -print0 | xargs --null)"
 
     sed -i '' 's/L = lua_newstate(hlua_alloc, \&hlua_global_allocator);/L = lua_newstate(hlua_alloc, \&hlua_global_allocator, luaL_makeseed(0));/' lua.h
+    echo $(grep "L = lua_newstate(hlua_alloc, &hlua_global_allocator);" lua.h)
+    echo $(ls)
+    echo "PIPISKA"
 
     case "${TARGET}" in
 
